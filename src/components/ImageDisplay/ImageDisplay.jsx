5760e5de8115966e410311b27cf32791d61bbbe6
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-10-29 22:49:58 +0200

    fix photo

--- src/components/ImageDisplay/ImageDisplay.jsx
+++ src/components/ImageDisplay/ImageDisplay.jsx
@@ -3,19 +3,21 @@ import styles from './ImageDisplay.module.scss';
 
 const ImageDisplay = () => {
   const imageKeywords = {
-    keyword1: 'карась',
-    keyword2: 'кобра',
-    keyword3: 'Дуб',
-    keyword4: 'міст',
-    keyword5: 'фонтан',
+    keyword1: 'джерело',
+    keyword2: 'змія',
+    keyword3: 'фонтан',
+    keyword4: 'потяг',
+    keyword5: 'спорт',
+    keyword6: 'замок',
   };
 
   const imageSources = {
-    keyword1: 'https://cdn.pixabay.com/photo/2019/10/02/20/18/koi-4521885_1280.jpg',
+    keyword1: 'https://3.bp.blogspot.com/-vpQ2dAyw37o/WnsGAgN1DvI/AAAAAAAAAGw/rZNUS7_O6Ns43ikhAGT80GI7Qc7Qh_ylgCEwYBhgL/s1600/37%25D0%25B1.jpg',
     keyword2: 'https://cdn.pixabay.com/photo/2014/07/30/19/28/king-cobra-405623_1280.jpg',
-    keyword3: 'https://cdn.pixabay.com/photo/2017/01/29/17/36/oak-tree-2018822_1280.jpg',
-    keyword4: 'https://cdn.pixabay.com/photo/2016/11/29/09/49/blonde-1868815_1280.jpg',
-    keyword5: 'https://cdn.pixabay.com/photo/2023/10/16/10/57/fountain-8318963_1280.jpg',
+    keyword3: 'https://cdn.pixabay.com/photo/2023/10/16/10/57/fountain-8318963_1280.jpg',
+    keyword4: 'https://cdn.pixabay.com/photo/2016/10/10/14/44/train-1728537_1280.jpg',
+    keyword5: 'https://cdn.pixabay.com/photo/2021/03/25/21/09/boxing-6124217_1280.jpg',
+    keyword6: 'https://cdn.pixabay.com/photo/2015/01/15/16/17/love-600488_1280.jpg',
   };
 
   const [inputValues, setInputValues] = useState({
@@ -24,6 +26,7 @@ const ImageDisplay = () => {
     input3: '',
     input4: '',
     input5: '',
+    input6: '',
   });
 
   const handleInputChange = (e, inputNumber) => {
@@ -38,7 +41,7 @@ const ImageDisplay = () => {
 
  return (
     <div className={styles.container}>
-      {[1, 2, 3, 4, 5].map((number) => (
+      {[1, 2, 3, 4, 5, 6].map((number) => (
         <div className={styles.inputWrapper} key={number}>
           <input
             className={styles.imageInput}
